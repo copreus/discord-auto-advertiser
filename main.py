380dcd9b0a0bd300e260ff712074ac372646b5b1
gustobusto <111027282+gustobusto@users.noreply.github.com>
2022-08-10 18:30:42 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+import httpx, time
+#Put your discord token in the brackets
+token = ""
+
+#Put what message you want sent in the brackets below
+content = "Hello"
+
+#This is where the message will be sent to. To get a channel ID, search youtube or google.
+channelid = ""
+
+#Delay before sending message again in SECONDS
+delay = 3
+
+while True:
+    time.sleep(delay)
+    ree = httpx.post(f'https://discord.com/api/v9/channels/{channelid}/messages', headers={'authorization': token}, json={"content": content})
+    if ree.status_code == 200:
+        print(f"Sent {content} to channel with ID {channelid}")
+    else:
+        print("Failed to send message! Invalid token perhaps?")
